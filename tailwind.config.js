e609dbbc320baea72cdcdb3003485a8030c28713
Peter Francis Robante <85429210+ipetersenpai@users.noreply.github.com>
2023-04-21 22:12:48 +0800

    build: Replaced npm dependencies to yarn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+    "./src/screens/Login.{js,jsx,ts,tsx}",
+    "./src/components/Background.{js,jsx,ts,tsx}",
   ],
+
   theme: {
     extend: {},
+    backgroundImage: {
+      "style-image": "url('./assets/style-image-1.png')",
+      burger: "url('./assets/burger.png')",
+      pizza: "url('./assets/pizza.png')",
+      fries: "url('./assets/fries.png')",
+    },
   },
   plugins: [],
 };
